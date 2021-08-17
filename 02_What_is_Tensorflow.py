edde933de3b7b3b4de52907aff170ce3a01f5e67
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-17 11:49:05 +0500

    1.0

---
+++ 02_What_is_Tensorflow.py
@@ -0,0 +1,40 @@
+# (02)*******************What is Tensorflow*******************************
+
+# ----Discuss About----
+# ----(1)-Mathematical Foundation
+# ----(2)-What is Tensor?
+# ----(3)-What is flow?
+# ----(4)-What is Tensorflow?
+# ----(5)-Study materials
+
+
+# *************Mathematical Foundation
+# ---Salar---Scalars are mathematical entities which have only a magnitude(and no direction)---Example----Volume,density,speed,energy,mass, and time
+# ---Vector---An array of numbers, which is wither continuous or discrete, is defined as a vector---Example----Force and Velocity, have both magnitude and direction and are called vector
+# ---Matrix can be defined as multi-dimentional arrays, which are arranged in the format of rows and columns
+
+
+# ************ What is Tensor of TensorFlow?
+# ----Tensor---A tensor is a mathematical object
+# ---Tensor are defined as multidimentional array or list.
+# ---Tensor are identifed by the following three parameters.
+# ---(1)-Rank:it identifies the number of dimensions of the tensor---Example---Scaler,vector,Matrix
+# ---(2)-Shape:Number of rows and columns of Tensor
+# ---(3)-Type:Data type assigned to Tensor's elements---Example---Integer,Float,String,Boolean
+# ---(4)-Scaler,Arrays or vectors, which are one dimensional, or matrices,Which are two-dimentional.But tensors can be more than two Dimention(Three,four,five dimentional)
+
+
+# *********What is Flow of Tensorflow?
+# ---Flow---Graphical representation of mathematical operation
+# ---Everything in TensorFlow is based on designing a computational graph.
+# ---The graph has a network of nodes,with each node operating addition,multiplication or evaluation some multivariate equation.
+# ---The code is written to build the graph, create a session and execute that graph
+# Matrix_A,Matrix_B------>>>Add A&B------->>>>C
+
+
+# *******What is TensorFlow?
+# ----TensorFlow is the core open source library to help youdevelop and train Machine Learning/Deep Learning Models
+# ----Develop by Google Brain Team
+# ----TensorFlow is Wriiten in Python,C++,CUDA
+# ----Platform:Linux,maxOS,Windows,Android,JavaScript
+# ----Initial release:November 9,2015 
