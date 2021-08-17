1d4c573c79058b5ac9320925819141cb68cc183e
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-17 10:34:32 +0500

    1.0

---
+++ Introduction_tensorflow.py
@@ -0,0 +1,58 @@
+# (01)***********************Introduction of Tensorflow*******************************
+
+# ----Discuss About----
+# ----(1)-What is Tensorflow
+# ----(2)-Application
+# ----(3)-Who can learn
+# ----(4)-Prerequiste
+# ----(5)-Dependency
+# ----(6)-Installation
+# ----(7)-Importing
+# ----(8)-Study materials
+
+
+# -----Whay is Tensorflow?
+# ----Definition----Tensorflow is the core open source library to help you develop and train Machine Learing/Deep Learning Models.
+# ----Develop by Google Brain Team
+# ----TensorFlow is written in Python, C++, CUDA
+# ----Platform:Linux,macOS,Windows,Andriod,JavaScript
+# ----Initial release:Novemver 9,2015.
+
+# ----Official website link----https://www.tensorflow.org/
+# ----Tensorflow use companies----Cocacola,Google,intel,tweeter,airbnb,DeepMind,GE Healtcare,NeRSC
+
+# ******Application
+# ----Face detection
+# ----Auto drive cars
+# ----Robotics
+# ----Alex Google Assistent
+
+
+# ************Who can learn tensorflow?
+# ----Pyhton
+# ----Machine Learning Engineer
+# ----Data Scientist
+# ----Artificial Intelligence Engineer
+# ----Deep learninh Engineer
+# ----Computer Vision/Natual language processing Engineer
+
+
+# ***********Why learn Tensorflow
+# ----Tensorflow is very helpful and very used in wordwide
+
+# *************Prerequisties/Dependency for Tensorflow
+# ----Python
+# ----Numpy
+# ----Scipy
+# ----Pandas
+# ----Matplotlib
+# ----Seaborn
+# ----Math:Matric,Vector,Calculus,Statistics,probability
+
+# ----Installation of Tensorflow
+# ----pip install tensorflow
+# ----------------for installing in gpu
+# ----pip install tensorflow-gpu
+
+# ----for importing the tensorflow
+import tensorflow as tf
