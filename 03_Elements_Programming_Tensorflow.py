43ac4ae05524b873d8bf7728ee430b88bdaeecf6
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-17 12:03:44 +0500

    1.0

---
+++ 03_Elements_Programming_Tensorflow.py
@@ -0,0 +1,24 @@
+# (03)***********************Elements of Programming in TensorFlow*****************
+
+# -------Discuss about-----
+# ---(1)---constant
+# ---(2)---variables
+# ---(3)---placeholders
+# ---(4)---Sparse Tensor 
+
+
+# *******Constant---
+# ----Creates a constant tensor from a tensor like object.
+# ---Syntax: tf.constant()
+
+# ******Variables*****
+# ----Variables allows us to add new trainable parametrs to the  graph
+# ----Syntax:tf.variable()
+
+# *****Placeholders*****
+# ----Placeholders allow us to feed data to a TensorFlow model from outside a model
+# ----Syntax:tf.placeholder()
+
+# *****Sparse Tensor
+# ----Sparse tensor enable efficient storage and processing of tensors that contain a lot of zero values
+# ----Syntax: tf.SparseTensor()
