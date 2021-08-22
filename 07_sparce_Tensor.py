782c44c17569e7a5bc4f311aa509efbacc9da26c
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-22 17:27:12 +0500

    1.0

---
+++ 07_sparce_Tensor.py
@@ -0,0 +1,9 @@
+# (07)*******************Sparce Tensor in Tensorflow*****************************
+
+# ----Definition-----Sparse tensor enable efficient storage and processing of tensor that contain a lot of zero values.
+# ---Syntax:----tf.SparseTensor
+
+# -----image is achully shaped in 0 or 1 numbers
+# -----When we converd the program to image then 1 is showes the black color and 0 is showes the white color
+# -----then we convert to bytes and kilo bites we we formula-----
+# -----shape=(8,17),dtype=int32 -----Size=(8X17X32)/8------544bytes=0.53KB
